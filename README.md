ba1c8de81a814aaf044d64a423691cb34e683014
noxcodes <samingosi97@gmail.com>
2025-05-25 02:59:38 +0300

    initial commit for the project

---
+++ README.md
@@ -0,0 +1,3 @@
+# AirBnB Clone
+This project looks to clone the AirBnB app
+This repo is specifically about cloning the console
---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" This module defines the base model for all the classes
+"""
+
+
+import uuid
+import datetime
+
+class BaseModel:
+    """ This class defines the common attributes and methods
+    All classes will inherit these methods
+    """
+
+    def __init__(self):
+        """ This method inmitilizes all objects
+        It just assigns the id and the time using uuid
+        """
+
+        self.id = str(uuid.uuid4())
+        self.create_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Define how the instance should be printed
+        """
+
+        string = f"[{type(self).__name__}] ({self.id}) self.__dict__"
+        return string
+
+    def save(self):
+        """ A function to save the instance and the time it is saved
+        It simply updates the instance attribute updated_at
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ returns a dictionary containing the
+        key/values of the __dict__ instance
+        """
+
+        
