622ffd4a6ed300177df48d33a202f417edea0152
noxcodes <samingosi97@gmail.com>
2025-07-08 01:35:33 -0700

    yet to convert ctreated_at to string in to_dict methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" This module defines the base model for all the classes
-"""
+#""" This module defines the base model for all the classes""
 
 
 import uuid
@@ -21,7 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new()
+            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                 '%Y-%m-%dT%H:%M:%S.%f')
@@ -51,8 +50,8 @@ class BaseModel:
         """
 
         my_dict = self.__dict__
-        my_dict['__class__'] = self.__class__.__name__
-        my_dict['created_at'] = my_dict['created_at'].isoformat()
-        my_dict['updated_at'] = my_dict['updated_at'].isoformat()
+        my_dict['__class__'] = str(self.__class__.__name__)
+        my_dict['created_at'] = str(self.created_at.isoformat())
+        my_dict['updated_at'] = str(self.updated_at.isoformat())
 
         return my_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,28 +25,32 @@ class FileStorage():
 
     def new(self, obj):
        """ It sets in the __objects an obj with the key classname.id
+           Use with caution since it does not check if the
+            function does not check the passed object is of the BaseModel class
        """
 
-       if not (isinstance(obj, BaseModel)):
-           return
+#       if not (isinstance(obj, BaseModel)):
+#       """I wanted to check if it's a basemodel class however
+#         I can't import the basemodel class since it'll create a circular import
+#         i.e. The base model class imports this class
+#           return
        key = f"{obj.__class__.__name__}.{obj.id}"
-       self.__objects[key] = obj
+       self.__objects[key] = obj.to_dict()
 
     def save(self):
         """ It serializes the __objects dict to the json file
         """
 
         with open(self.__file_path, mode='w+') as fildes:
-            json.dump(self.__objects, self.__file_path)
+            json.dump(self.__objects, fildes)
 
     def reload(self):
         """ Deserializes the json file
         """
 
         try:
-            temp = {}
             with open(self.__file_path, 'r') as fildes:
                 temp = json.load(fildes)
-                self.__objects.append(temp)
+                self.__objects.update(temp)
         except:
                 pass
