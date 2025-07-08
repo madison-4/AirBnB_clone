0625ddd553960ae3f920aaf6f807377d5876a550
noxcodes <samingosi97@gmail.com>
2025-07-08 04:15:36 -0700

    create test module

---
+++ tests/test_file_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This is a module to test the functions added 
+in the file_storage module/.
+The save, reload, all and new functions
+"""
+
+import json
+import unittest
+
+
+class test_file_storage():
+    
