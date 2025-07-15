c536e026c08873faf47659306e75495f64bd064c
noxcodes <samingosi97@gmail.com>
2025-07-15 02:33:05 -0700

    add create command to console

--- console.py
+++ console.py
@@ -4,6 +4,8 @@
 
 import cmd
 import sys
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,8 +13,11 @@ class HBNBCommand(cmd.Cmd):
     console.
     """
 
+    classes = ['BaseModel', 'FileStorage']
     prompt = '(hbnb) '
-
+    nexist= "** class doesn't exist **"
+    miss = "** class name missing **"
+    
     def do_EOF(self, line):
         """ Exits the interorter gracefully
         """
@@ -36,6 +41,27 @@ class HBNBCommand(cmd.Cmd):
 
         pass
 
+    def help_quit():
+        """The help bar for the quit command
+        """
+        
+        print("quits the interpreter")
+
+    def do_create(self, line):
+        """A command to create an object of a class specified by line
+        """
+
+        if not line:
+            print(self.miss)
+        else:
+            comms = line.split()
+            cla = comms[0]
+            if cla not in (self.classes):
+                print(self.nexist)
+            else:
+                obj = eval(cla)()
+                obj.save()
+        
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
