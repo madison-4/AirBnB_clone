36ffad988f735bc79fb06adf279f5c6639d45351
noxcodes <samingosi97@gmail.com>
2025-08-04 12:25:14 +0300

    implement the update command

--- console.py
+++ console.py
@@ -3,12 +3,64 @@
 """
 
 import models
+import re
 import cmd
 import sys
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from shlex import split
 
+
+# A global constant since both functions within and outside uses it.
+CLASSES = [
+    "BaseModel",
+    "User",
+    "City",
+    "Place",
+    "State",
+    "Amenity",
+    "Review"
+]
+
+def parse(arg):
+    """ function to strip arguments and m,ake them able to strip functions
+    It simply removes nonimportant xchars
+    """
+
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
+
+def check_args(args):
+    """checks if args is valid
+
+    Args:
+        args (str): the string containing the arguments passed to a command
+
+    Returns:
+        Error message if args is None or not a valid class, else the arguments
+    """
+    arg_list = parse(args)
+
+    if len(arg_list) == 0:
+        print("** class name missing **")
+    elif arg_list[0] not in CLASSES:
+        print("** class doesn't exist **")
+    else:
+        return arg_list
 
 class HBNBCommand(cmd.Cmd):
     """A commandline interpreter to test the AirBnB
@@ -167,6 +219,43 @@ class HBNBCommand(cmd.Cmd):
                 print([str(obj) for obj in objects
                        if arg_list[0] in str(obj)])
 
+
+    def help_update(self):
+        """ update attributes in the object
+        """
+
+        pr_str = """This function changes, adds or removes\
+        the value of partuicular attributes for an instance\
+        Usage: update Class instanceid attribute name attriobute value
+        """
+        print(pr_str)
+
+    def do_update(self, line):
+        """ command to update attribute name
+        """
+
+        arg_list = check_args(line)
+        if arg_list:
+            if len(arg_list) == 1:
+                print("** instance id missing **")
+            else:
+                instance_id = "{}.{}".format(arg_list[0], arg_list[1])
+                if instance_id in self.storage.all():
+                    if len(arg_list) == 2:
+                        print("** attribute name missing **")
+                    elif len(arg_list) == 3:
+                        print("** value missing **")
+                    else:
+                        obj = self.storage.all()[instance_id]
+                        if arg_list[2] in type(obj).__dict__:
+                            v_type = type(obj.__class__.__dict__[arg_list[2]])
+                            setattr(obj, arg_list[2], v_type(arg_list[3]))
+                        else:
+                            setattr(obj, arg_list[2], arg_list[3])
+                else:
+                    print("** no instance found **")
+
+            self.storage.save()
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
